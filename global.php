6ab11d84f3bab84928bcc7fcda3e90c08ddad935
hamstar <hamstar@telescum.co.nz>
2012-05-04 07:30:36 +1200

    Update global.php

--- global.php
+++ global.php
@@ -1,6 +1,6 @@
 <?php
 
-include "vendor/markdown.php"; # http://michelf.com/docs/projets/php-markdown-1.0.1o.zip
+include "vendor/michelfortin/php-markdown/markdown.php"; # http://michelf.com/docs/projets/php-markdown-1.0.1o.zip
 include 'Formatter.php';
 include 'vendor/hugochinchilla/curl/curl.php'; # https://github.com/hugochinchilla/curl
 include 'ReqsBuilder.php';
