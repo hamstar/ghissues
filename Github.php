c5b01cccef06078dadfdf20dbfee1c7568526a89
Robert McLeod <hamstar@behemoth.(none)>
2012-06-26 23:45:16 +1200

    made the Github API wrapper handle multiple pages of issues

--- Github.php
+++ Github.php
@@ -6,16 +6,39 @@ class Github {
 
 	function __construct( $repo_string, Curl $curl ) {
 		
-		$repo = explode( ":", $repo_string );
-		$this->api_url.= "/{$repo[0]}/{$repo[1]}";
 		list( $user, $repo ) = explode( ":", $repo_string );
 		$this->api_url.= "/$user/$repo";
 		$this->curl = $curl;
 	}
 
+	/**
+	* @return array all the issues for the current repo
+	*/
 	function issues() {
 		
-		$json = $this->curl->get( $this->api_url . '/issues')->body;
-		return json_decode( $json );
+		$issues = array();
+		$page_has_issues = TRUE;
+		$page = 1;
+
+		// Get every page of issues until there's none left
+		while ( $page_has_issues == TRUE ) {
+		
+			// Call the api and decode the issues
+			$url = $this->api_url . "/issues?page=$page";
+			$json = $this->curl->get( $url )->body;
+			$_issues = json_decode( $json );
+
+			// No issues on this page? jump out
+			if ( count( $_issues ) == 0 ) {
+				$page_has_issues = FALSE; // extra protection from infinite loop
+				break;
+			}
+
+			// Add the issues to the ones we already have
+			$issues = array_merge( $issues, $_issues );
+			++$page; // and hit the next page on the next loop
+		}
+
+		return $issues;
 	}
 }
