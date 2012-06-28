3b597178f4c2b230410237f6af12b1924c6fc679
Robert McLeod <hamstar@behemoth.(none)>
2012-06-28 22:37:34 +1200

    made the index print issues if called with argument ?verbose

--- index.php
+++ index.php
@@ -9,6 +9,12 @@ $gh = new Github( "hamstar:Braincase", new Curl )
 $issues = $gh->get_issues();
 echo round( microtime(true) - $start, 2 )." seconds to get issues<br/><br/>";
 
+if ( isset( $_GET['verbose'] ) ) { // print the features one per line
+	echo "Printing feature list:<br/>";
+	Github::print_issues( $issues );
+	echo "<br/>";
+}
+
 $start = microtime(true);
 $builder = new FeatureBuilder( $issues );
 $features = $builder->get_features();
