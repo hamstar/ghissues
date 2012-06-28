b334d7def97f50e8626683a16df4e5a75c5e25f3
Robert McLeod <hamstar@behemoth.(none)>
2012-06-28 22:56:23 +1200

    changed Formatter to IssueFormatter

--- global.php
+++ global.php
@@ -1,7 +1,7 @@
 <?php
 
 include "vendor/michelfortin/php-markdown/markdown.php"; # http://michelf.com/docs/projets/php-markdown-1.0.1o.zip
-include 'Formatter.php';
+include 'IssueFormatter.php';
 include 'vendor/hugochinchilla/curl/curl.php'; # https://github.com/hugochinchilla/curl
 include 'FeatureBuilder.php';
 include 'Github.php';
