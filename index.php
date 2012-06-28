490610638837ec4829e9b47cc5164681a1e476fc
Robert McLeod <hamstar@behemoth.(none)>
2012-06-28 15:59:16 +1200

    made changes to index to reflect the new interfaces

--- index.php
+++ index.php
@@ -4,8 +4,16 @@
 
 include 'global.php';
 
-$builder = new ReqsBuilder( new Github( "hamstar:Braincase", new Curl ) );
-$builder->run();
+$start = microtime(true);
+$gh = new Github( "hamstar:Braincase", new Curl )
+$issues = $gh->get_issues();
+echo round( microtime(true) - $start, 2 )." seconds to get issues<br/><br/>";
 
-$fmtr = new IssueFormatter( $builder );
+$start = microtime(true);
+$builder = new FeatureBuilder( $issues );
+$features = $builder->get_features();
+echo round( microtime(true) - $start, 2 )." seconds to process features<br/><br/>";
+
+echo "Printing issues...<br/><br/>";
+$fmtr = new IssueFormatter( $features );
 echo Markdown( $fmtr->get_markdown() );
